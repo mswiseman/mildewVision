c2dae2a4db048f49ad7dba97a4b7a0bf096ce7f7
Michele Wiseman <33985124+mswiseman@users.noreply.github.com>
2023-07-27 07:53:30 -0700

    Update create_gif.py

--- aps2023_visuals/create_gif.py
+++ aps2023_visuals/create_gif.py
@@ -1,41 +1,21 @@
-import imageio
+import numpy as np
+import imageio.v2 as imageio
 import os
-from PIL import Image, ImageSequence
 
-def create_gif(image_files, output_file, duration, fade_duration):
-    """
-    Create a GIF from a list of image files, with control over the cycle speed and a fading effect
-    between frames.
 
-    Args:
-        image_files: List of paths to the input image files.
-        output_file: Path to the output GIF file.
-        duration: Duration to display each frame, in seconds.
-        fade_duration: Duration of the fade effect between frames, in seconds.
-    """
-  
-    # Create a list to hold the frames of the GIF
+def create_morph_gif(image_files, output_file, steps):
+    images = [imageio.imread(file) for file in image_files]
+
     frames = []
+    for i in range(len(images)):
+        for t in np.linspace(0, 1, steps):
+            morphed_image = (1 - t) * images[i] + t * images[(i + 1) % len(images)]
+            frames.append(morphed_image.astype(np.uint8))
 
-    # Load each image file
-    for filename in image_files:
-        image = Image.open(filename)
-
-        # If fade_duration is greater than 0, create a fading effect
-        if fade_duration > 0:
-            # Create a sequence of images that gradually fade in the next image
-            fade_sequence = ImageSequence.Iterator(
-                Image.blend(image, next_image, alpha)
-                for alpha in np.linspace(0, 1, int(fade_duration / duration))
-            )
-
-            # Add the fade sequence to the list of frames
-            frames.extend(list(fade_sequence))
-        else:
-            frames.append(image)
-
-    # Create the GIF
-    imageio.mimsave(output_file, frames, 'GIF', duration=duration)
+    imageio.mimsave(output_file, frames, 'GIF', duration=0.1)
 
 # Usage:
-# create_gif(["image1.png", "image2.png", "image3.png"], "output.gif", 0.5, 0.1)
+directory = '/Users/michelewiseman/Desktop/aps_visuals/'
+os.chdir(directory)  # change the current working directory to the directory where the images are stored
+create_morph_gif(["Picture1.png", "Picture2.png", "Picture3.png", "Picture4.png", "Picture6.png"],
+                 "mildew_slow.gif", 15)
