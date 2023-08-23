457cecabc51ab04f5c041d188d9e1af16b02f5ef
Michele Wiseman <33985124+mswiseman@users.noreply.github.com>
2023-08-23 16:36:20 -0700

    Update patch_sr.py
    included conidiophore patches in severity rating and some print statements for troubleshooting.

--- metric/patch_sr.py
+++ metric/patch_sr.py
@@ -20,23 +20,33 @@ def metric(patch_info, heatmap_info, threshold_info):
     infected_pixel1 = len(prob_heatmap1[prob_heatmap1 >= patch_up_th])
     clear_pixel1 = len(prob_heatmap1[prob_heatmap1 <= patch_down_th]) - len(prob_heatmap1[prob_heatmap1 == -np.inf])
 
+    # Uncomment for troubleshooting
+    # print("patch_up_th: ", patch_up_th)
+    # print("patch_down_th: ", patch_down_th)
+    # print("infected_pixel: ", infected_pixel)
+    # print("clear_pixel: ", clear_pixel)
+    # print("infected_patch: ", infected_patch)
+    # print("conidiophores_patch", conidiophores_patch)
+    # print("clear_patch: ", clear_patch)
+    # print("patch_sr: ", round((infected_patch / (infected_patch + clear_patch)) * 100, 2))
+
     return round((infected_patch / (infected_patch + clear_patch)) * 100, 2), (infected_pixel1, clear_pixel1)
 
 
 def metric_two_class(patch_info, heatmap_info, threshold_info):
     prob_heatmap1 = heatmap_info['prob_heatmap1']  # infected class probability heatmap
     prob_heatmap2 = heatmap_info['prob_heatmap2']  # conidiophore class probability heatmap
+
     patch_down_th = threshold_info['patch_down_th']
     patch_up_th = threshold_info['patch_up_th']
 
     infected_patch = patch_info['infected_patch']
+    conidiophores_patch = patch_info['conidiophores_patch']
     clear_patch = patch_info['clear_patch']
 
     # Process prob_heatmap1
     infected_pixel1 = len(prob_heatmap1[prob_heatmap1 >= patch_up_th])
     clear_pixel1 = len(prob_heatmap1[prob_heatmap1 <= patch_down_th]) - len(prob_heatmap1[prob_heatmap1 == -np.inf])
-    #print("patch_up_th: ", patch_up_th)
-    #print("patch_down_th: ", patch_down_th)
 
     # Process prob_heatmap2
     infected_pixel2 = len(prob_heatmap2[prob_heatmap2 >= patch_up_th])
@@ -47,11 +57,15 @@ def metric_two_class(patch_info, heatmap_info, threshold_info):
             clear_pixel1 + clear_pixel2 + infected_pixel1 + infected_pixel2)
     clear_pixel = (clear_pixel1 + clear_pixel2) / (clear_pixel1 + clear_pixel2 + infected_pixel1 + infected_pixel2)
 
-    #print("infected_pixel: ", infected_pixel)
-    #print("clear_pixel: ", clear_pixel)
-    #print("infected_patch: ", infected_patch)
-    #print("clear_patch: ", clear_patch)
-
+    # Uncomment for troubleshooting
+    # print("patch_up_th: ", patch_up_th)
+    # print("patch_down_th: ", patch_down_th)
+    # print("infected_pixel: ", infected_pixel)
+    # print("clear_pixel: ", clear_pixel)
+    # print("infected_patch: ", infected_patch)
+    # print("conidiophores_patch", conidiophores_patch)
+    # print("clear_patch: ", clear_patch)
+    # print("patch_sr: ", round(((infected_patch + conidiophores_patch) / (infected_patch + clear_patch + conidiophores_patch)) * 100, 2))
 
     # returns the percentage of infected pixels and the number of infected and clear pixels
-    return round((infected_patch / (infected_patch + clear_patch)) * 100, 2), (infected_pixel, clear_pixel)
+    return round(((infected_patch + conidiophores_patch) / (infected_patch + conidiophores_patch + clear_patch)) * 100, 2), (infected_pixel, clear_pixel)
