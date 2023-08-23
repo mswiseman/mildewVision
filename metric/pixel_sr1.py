8569b7fbcb71661c7c343149bc55eef70825a866
Michele Wiseman <33985124+mswiseman@users.noreply.github.com>
2023-08-23 16:39:26 -0700

    Update pixel_sr1.py

--- metric/pixel_sr1.py
+++ metric/pixel_sr1.py
@@ -4,27 +4,33 @@ import numpy as np
 sys.path.append('..')
 
 from utils import hard_thresholding, otsu_thresholding
-
 from analyzer_config import IMG_HEIGHT, IMG_WIDTH
-
 
 def metric(patch_info, heatmap_info, threshold_info, outdim):
 
     """
-        Calculate pixel level severity rate using gradcam heatmap
-    Args:
-        patch_info:         patch related information
-        heatmap_info:       heatmap related information
-        threshold_info:     threshold related information
-        outdim:             output dimension of the model
-    """
+    Calculate pixel-level severity rate using a gradcam heatmap.
 
+    Args:
+    - patch_info (dict): Contains patch-related information.
+    - heatmap_info (dict): Contains heatmap-related information.
+    - threshold_info (dict): Contains threshold-related information.
+    - outdim (int): Output dimension of the model.
+
+    Returns:
+    - dict: Severity rates for each threshold and key.
+    - tuple: Infected pixels and total pixels.
+    """
+    # Remove 'prob_heatmap[x]' if exists
     heatmap_info.pop('prob_heatmap', None)
+    heatmap_info.pop('prob_heatmap1', None)
+    heatmap_info.pop('prob_heatmap2', None)
 
     lost_focus_patch = patch_info['lost_focus_patch']
-    lost_focus_pixel = lost_focus_patch * IMG_HEIGHT * IMG_WIDTH
-
+    lost_focus_pixel = patch_info['lost_focus_patch'] * IMG_HEIGHT * IMG_WIDTH
     pixel_th = threshold_info['pixel_th']
+    # print("lost_focus_patch: ", lost_focus_patch)
+
 
     severity_rates = {}
     infected_pixels = {}
@@ -32,30 +38,22 @@ def metric(patch_info, heatmap_info, threshold_info, outdim):
 
     for th in pixel_th:
         for key, val in heatmap_info.items():
-            if th == 'otsu':
-                saliency_mask = otsu_thresholding(val, vmin=0, vmax=1)
-            else:
-                saliency_mask = hard_thresholding(val, float(th), vmin=0, vmax=1)
+            saliency_mask = otsu_thresholding(val, vmin=0, vmax=1) if th == 'otsu' else hard_thresholding(val,
+                                                                                                          float(th),
+                                                                                                          vmin=0,
+                                                                                                          vmax=1)
 
-            total_pixel = saliency_mask.shape[0] * saliency_mask.shape[1]
-            clear_pixel = len(saliency_mask[saliency_mask == 0])
-
-            if outdim == 3:  # combine infected and conidiophores
-                infected_pixel = np.count_nonzero((saliency_mask == 1) | (saliency_mask == 2))
-            else:
-                infected_pixel = len(saliency_mask[saliency_mask == 1])
+            total_pixel = saliency_mask.size
+            clear_pixel = np.sum(saliency_mask == 0)
+            infected_pixel = np.sum(saliency_mask == 1) + (np.sum(saliency_mask == 2) if outdim == 3 else 0)
 
             assert clear_pixel + infected_pixel == total_pixel
 
-            if not severity_rates.get(th, None):
-                severity_rates[th] = {}
-            severity_rates[th][key] = round(infected_pixel / (total_pixel - lost_focus_pixel) * 100, 2)
+            severity_rates.setdefault(th, {})[key] = round(infected_pixel / (total_pixel - lost_focus_pixel) * 100, 2)
+            infected_pixels.setdefault(th, {})[key] = infected_pixel
+            total_pixels.setdefault(th, {})[key] = total_pixel - lost_focus_pixel
 
-            if not infected_pixels.get(th, None):
-                infected_pixels[th] = {}
-            infected_pixels[th][key] = infected_pixel
-            if not total_pixels.get(th, None):
-                total_pixels[th] = {}  #
-            total_pixels[th][key] = total_pixel - lost_focus_pixel
+            # print("key: ", key)
+            # print("severity_rates: ", severity_rates)
 
-    return severity_rates, (infected_pixel, total_pixel)
+        return severity_rates, (infected_pixel, total_pixel)
