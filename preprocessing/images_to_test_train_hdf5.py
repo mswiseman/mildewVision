62ad864dc9f90cfd9178a1dde77edd44ff8a1878
Michele Wiseman <33985124+mswiseman@users.noreply.github.com>
2024-01-25 12:44:51 -0800

    Update images_to_test_train_hdf5.py
    added class balancing arg

--- preprocessing/images_to_test_train_hdf5.py
+++ preprocessing/images_to_test_train_hdf5.py
@@ -7,6 +7,7 @@ import numpy as np
 import hashlib
 from PIL import Image
 from sklearn.model_selection import KFold
+from sklearn.model_selection import train_test_split
 from sklearn.utils import shuffle
 
 """
@@ -34,22 +35,26 @@ Variables:
 - blur_threshold_factor: A factor subtracted from the median variance of Laplacian to determine the threshold for removing blurry images.
 - k_fold: Whether to perform k-fold cross validation.
 - k_fold_number: The number of folds for k-fold cross validation.
+- balanced: Whether to balance the classes in the dataset (adjusts to smallest class size).
 
 To use:
 python images_to_test_train_hdf5.py -sample_size 100 -blur_threshold_factor 30 -image_size 224 224 -input_dir /Users/michelewiseman/Desktop/test -output_dir /Users/michelewiseman/Desktop/test
 
 Written by Michele Wiseman of Oregon State University
+v2
 """
 
 # Add argparse arguments
 parser = argparse.ArgumentParser(description="Preprocessing images script")
 parser.add_argument('-sample_size', type=int, default=1000, help="Number of images used to calculate the median threshold")
-parser.add_argument('-blur_threshold_factor', type=int, default=100, help="Factor subtracted from the median variance of Laplacian to determine the threshold for removing blurry images")
+parser.add_argument('-blur_threshold_factor', type=int, default=140, help="Factor subtracted from the median variance of Laplacian to determine the threshold for removing blurry images")
 parser.add_argument('-image_size', type=int, default=(224, 224), nargs=2, help="The size to which all images are resized")
-parser.add_argument('-input_dir', type=str, default='/Users/michelewiseman/Desktop/downy_model', help="Directory where the input images are located")
-parser.add_argument('-output_dir', type=str, default='/Users/michelewiseman/Desktop/downy_model', help="Directory where the output HDF5 files are saved")
+parser.add_argument('-input_dir', type=str, default='/Users/michelewiseman/Desktop/mapping_population_patches_10dpi_model', help="Directory where the input images are located")
+parser.add_argument('-output_dir', type=str, default='/Users/michelewiseman/Desktop/mapping_population_patches_10dpi_model', help="Directory where the output HDF5 files are saved")
 parser.add_argument('-k_fold', action='store_true', help="Perform k-fold cross validation")
 parser.add_argument('-k_fold_number', type=int, default=5, help="Number of folds for k-fold cross validation")
+parser.add_argument('-balanced', action='store_true', help="Balance classes in the dataset")
+
 args = parser.parse_args()
 
 SEED = 42
@@ -180,6 +185,26 @@ print(f"Number of infected labels: {np.count_nonzero(labels == 1)}")
 print(f"Number of conidiophores labels: {np.count_nonzero(labels == 2)}")
 print(f'..................................................')
 
+# After counting the labels
+if args.balanced:
+    print('Balancing dataset')
+    # Find the minimum size among the classes to balance around
+    min_size = min(np.count_nonzero(labels == 0), np.count_nonzero(labels == 1), np.count_nonzero(labels == 2))
+    balanced_images = []
+    balanced_labels = []
+    for label_num in range(3):  # Assuming three classes
+        # Find indices of the current class
+        indices = np.where(labels.flatten() == label_num)[0]
+        # Randomly choose `min_size` indices from this class
+        chosen_indices = np.random.choice(indices, min_size, replace=False)
+        # Append chosen images and labels to the balanced lists
+        balanced_images.extend(images[chosen_indices])
+        balanced_labels.extend(labels[chosen_indices])
+    # Convert lists back to numpy arrays
+    images = np.array(balanced_images)
+    labels = np.array(balanced_labels)
+    print(f'Balanced dataset size: {len(images)}')
+
 # Shuffling and creating hdf5
 print('Shuffling images and creating hdf5')
 indices = np.arange(len(images))
@@ -193,18 +218,16 @@ if args.k_fold:
     kf = KFold(n_splits=k_folds)
 
     for fold, (train_indices, test_indices) in enumerate(kf.split(images)):
-        train_data, test_data = images[train_indices], images[test_indices]
-        train_labels, test_labels = labels[train_indices], labels[test_indices]
+        # Split the test set into test and validation sets
+        test_indices, val_indices = train_test_split(test_indices, test_size=0.5)
 
-        # Do the same for validation set if you wish
-        # Or split the test set into half for test and validation sets
-        # val_data = ...
-        # val_labels = ...
+        train_data, test_data, val_data = images[train_indices], images[test_indices], images[val_indices]
+        train_labels, test_labels, val_labels = labels[train_indices], labels[test_indices], labels[val_indices]
 
         # Print the shape of each split
         print(f"Training set shape for fold {fold}: {train_data.shape}, {train_labels.shape}")
         print(f"Testing set shape for fold {fold}: {test_data.shape}, {test_labels.shape}")
-        # print(f"Validation set shape for fold {fold}: {val_data.shape}, {val_labels.shape}")
+        print(f"Validation set shape for fold {fold}: {val_data.shape}, {val_labels.shape}")
         print(f'..................................................')
 
         # Save the datasets to new HDF5 files
@@ -219,46 +242,49 @@ if args.k_fold:
             test_f.create_dataset('labels', data=test_labels)
 
         # Uncomment the following if you create validation sets
-        # with h5py.File(os.path.join(output_dir, f'val_{fold}.hdf5'), 'w') as val_f:
-        #     val_f.create_dataset('images', data=val_data)
-        #     val_f.create_dataset('labels', data=val_labels)
-
-    else:
-        print('Splitting into test/train/val datasets')
-        num_train = int(0.8 * len(indices))
-        num_test = int(0.1 * len(indices))
-        num_val = len(indices) - num_train - num_test
-
-        indices_train = indices[:num_train]
-        indices_test = indices[num_train:num_train + num_test]
-        indices_val = indices[num_train + num_test:]
-
-        train_data = images[indices_train]
-        train_labels = labels[indices_train]
-
-        test_data = images[indices_test]
-        test_labels = labels[indices_test]
-
-        val_data = images[indices_val]
-        val_labels = labels[indices_val]
-
-        # Printing the shape of each split
-        print(f"Training set shape: {train_data.shape}, {train_labels.shape}")
-        print(f"Testing set shape: {test_data.shape}, {test_labels.shape}")
-        print(f"Validation set shape: {val_data.shape}, {val_labels.shape}")
-        print(f'..................................................')
-
-        # Save the datasets to new HDF5 files
-        print('Saving to HDF5 files')
-
-        with h5py.File(os.path.join(output_dir, f'train.hdf5'), 'w') as train_f:
-            train_f.create_dataset('images', data=train_data)
-            train_f.create_dataset('labels', data=train_labels)
-
-        with h5py.File(os.path.join(output_dir, f'test.hdf5'), 'w') as test_f:
-            test_f.create_dataset('images', data=test_data)
-            test_f.create_dataset('labels', data=test_labels)
-
-        with h5py.File(os.path.join(output_dir, f'val.hdf5'), 'w') as val_f:
+        with h5py.File(os.path.join(output_dir, f'val_{fold}.hdf5'), 'w') as val_f:
             val_f.create_dataset('images', data=val_data)
             val_f.create_dataset('labels', data=val_labels)
+
+
+else:
+    print('Splitting into test/train/val datasets')
+    # First, split the data into a training set and a temporary set (combining test and validation)
+    train_data, temp_data, train_labels, temp_labels = train_test_split(
+        images, labels, test_size=0.2, stratify=labels, random_state=SEED)
+
+    # Then, split the temporary set into test and validation sets
+    test_data, val_data, test_labels, val_labels = train_test_split(
+        temp_data, temp_labels, test_size=0.5, stratify=temp_labels, random_state=SEED)
+
+    # Now, you have your datasets ready for training, testing, and validation with balanced class distribution
+    print(f"Training set shape: {train_data.shape}, labels shape: {train_labels.shape}")
+    print(f"Testing set shape: {test_data.shape}, labels shape: {test_labels.shape}")
+    print(f"Validation set shape: {val_data.shape}, labels shape: {val_labels.shape}")
+
+
+    # Optionally, print the class distribution in each set to verify the stratification
+    def print_class_distribution(labels, split_name):
+        unique, counts = np.unique(labels, return_counts=True)
+        class_distribution = dict(zip(unique, counts))
+        print(f"{split_name} class distribution: {class_distribution}")
+
+
+    print_class_distribution(train_labels, "Training")
+    print_class_distribution(test_labels, "Testing")
+    print_class_distribution(val_labels, "Validation")
+
+    # Save the datasets to new HDF5 files
+    print('Saving to HDF5 files')
+
+    with h5py.File(os.path.join(output_dir, 'train.hdf5'), 'w') as train_f:
+        train_f.create_dataset('images', data=train_data)
+        train_f.create_dataset('labels', data=train_labels)
+
+    with h5py.File(os.path.join(output_dir, 'test.hdf5'), 'w') as test_f:
+        test_f.create_dataset('images', data=test_data)
+        test_f.create_dataset('labels', data=test_labels)
+
+    with h5py.File(os.path.join(output_dir, 'val.hdf5'), 'w') as val_f:
+        val_f.create_dataset('images', data=val_data)
+        val_f.create_dataset('labels', data=val_labels)
