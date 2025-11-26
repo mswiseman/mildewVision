3e94781374b457a1ec046e5a531cbca95a7491ec
Michele Wiseman <33985124+mswiseman@users.noreply.github.com>
2025-11-25 18:02:17 -0800

    Refactor command execution in plot_leaf_correlation_all.sh
    Refactor commands to use multi-line syntax for readability and maintainability.

--- plot_leaf_correlation_all.sh
+++ plot_leaf_correlation_all.sh
@@ -20,10 +20,52 @@ fi
 # Limit number of concurrent jobs
 MAX_JOBS=2
 
+# Commands to run (each in its own timed Python process)
 commands=(
-"time python ../plot_sal_map_leaf.py --model_type ResNet --model_path ../.. --dataset_path /e/Stacked/2025_AtTKO_HlMLO_Pheno --loading_epoch 59 --threshold 0.7 --up_threshold 0.8 --down_threshold 0.3 --cuda --cuda_id 0 --outdim 2 --means 0.5410 0.6371 0.4188 --stds 0.1764 0.1650 0.2326 --timestamp Feb14_15-53-04_2024 --dpi 2 --pretrained --sal_gradient --sal_deeplift --img_folder 10-3-2025_2dpi --trays Gc_USC1 --pm Gc_USC1"
-"time python ../leaf_correlation_mw.py --model_type ResNet --model_path ../.. --dataset_path /d/Stacked/Quintec_PM_Resistance_Screens_stained --loading_epoch 59 --threshold 0.7 --up_threshold 0.8 --down_threshold 0.3 --cuda --cuda_id 0 --outdim 2 --means 0.5410 0.6371 0.4188 --stds 0.1764 0.1650 0.2326 --timestamp Feb14_15-53-04_2024 --dpi 10 --pretrained --img_folder 11-24-2025_10dpi --trays 1 --sal_gradient --pm various"
+    "time \"$PYTHON\" ../plot_sal_map_leaf.py \
+        --model_type ResNet \
+        --model_path ../.. \
+        --dataset_path /e/Stacked/2025_AtTKO_HlMLO_Pheno \
+        --loading_epoch 59 \
+        --threshold 0.7 \
+        --up_threshold 0.8 \
+        --down_threshold 0.3 \
+        --cuda \
+        --cuda_id 0 \
+        --outdim 2 \
+        --means 0.5410 0.6371 0.4188 \
+        --stds 0.1764 0.1650 0.2326 \
+        --timestamp Feb14_15-53-04_2024 \
+        --dpi 2 \
+        --pretrained \
+        --sal_gradient \
+        --sal_deeplift \
+        --img_folder 10-3-2025_2dpi \
+        --trays 1 \
+        --pm Gc_USC1"
+
+    "time \"$PYTHON\" ../leaf_correlation_mw.py \
+        --model_type ResNet \
+        --model_path ../.. \
+        --dataset_path /d/Stacked/Quintec_PM_Resistance_Screens_stained \
+        --loading_epoch 59 \
+        --threshold 0.7 \
+        --up_threshold 0.8 \
+        --down_threshold 0.3 \
+        --cuda \
+        --cuda_id 0 \
+        --outdim 2 \
+        --means 0.5410 0.6371 0.4188 \
+        --stds 0.1764 0.1650 0.2326 \
+        --timestamp Feb14_15-53-04_2024 \
+        --dpi 10 \
+        --pretrained \
+        --img_folder 11-24-2025_10dpi \
+        --trays 1 \
+        --sal_gradient \
+        --pm various"
 )
+
 wait_for_free_job_slot() {
     while : ; do
         jobs_running=$(jobs -r | wc -l)
@@ -34,9 +76,11 @@ wait_for_free_job_slot() {
     done
 }
 
+# Launch commands, respecting MAX_JOBS
 for cmd in "${commands[@]}"; do
     eval "$cmd" &
     wait_for_free_job_slot
 done
 
+# Wait for all background jobs to complete
 wait
