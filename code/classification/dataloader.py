Fix label mapping and saliency mask computation
Corrected 'conidiophores' to 'conidiophore' in label_class_map and ensured labels are reshaped and cast to int64 in HyphalDataset. Fixed typo in saliency mask computation by replacing 'np.m
ultiply' with 'np.multiply' for 'GBP-GC' key.

code/classification/dataloader.py
@@ -11,7 +11,7 @@ import torchvision
     
 class HyphalDataset(torch.utils.data.Dataset):
 
-    label_class_map = {0: 'clear', 1: 'infected', 2: 'conidiophores'}
+    label_class_map = {0: 'clear', 1: 'infected', 2: 'conidiophore'}
 
     def __init__(self, dataset_path, train=True, transform=None, target_transform=None):
         self.root_dir = Path(dataset_path['root_path'])
@@ -35,7 +35,7 @@ class HyphalDataset(torch.utils.data.Dataset):
             image_ds = f['images']
             self.images = image_ds[:, ]
             label_ds = f['labels']
-            self.labels = label_ds[:]
+            self.labels = label_ds[:].reshape(-1).astype(np.int64)
 
     def __len__(self):
         return len(self.labels)

code/sanity_check/utils.py
@@ -408,8 +408,7 @@ def get_saliency_masks(saliency_methods, input_img, logits_class, relu_attributi
         output_masks[key] = attr.transpose(1, 2, 0)
 
     if 'Guided\nBackProp' in output_masks.keys():
-        output_masks['GBP-GC'] = np.m
-        ultiply(
+        output_masks['GBP-GC'] = np.multiply(
             output_masks['Guided\nBackProp'], output_masks['GradCAM'])
 
     return output_masks