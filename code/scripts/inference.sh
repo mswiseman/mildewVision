2e4ffb26562c8a6e1e84936e1d2e7de7999a3b1e
Michele Wiseman <33985124+mswiseman@users.noreply.github.com>
2026-01-22 01:07:26 -0800

    Update inference script parameters and paths

--- code/scripts/inference.sh
+++ code/scripts/inference.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 
-
-python3 ../classification/inference.py                                          \
+python ../classification/inference.py                                           \
             --model_path     ../..                                              \
-            --dataset_path   ../../data                                         \
+            --dataset_path   ../..                                              \
             --model_type ResNet                                                 \
             --HDF5                                                              \
+            --amp                                                               \
             --pretrained                                                        \
             --set test                                                          \
+            --dual_head                                                         \
+            --inf_gate 0.2                                                      \
+            --up_threshold 0.7                                                  \
+            --down_threshold 0.05                                               \
+            --spor_th 0.85                                                      \
             --cuda                                                              \
             --cuda_id 0                                                         \
-            --loading_epoch 59                                                  \
-            --timestamp Feb14_15-53-04_2024                                     \
+            --loading_epoch 43                                                  \
+            --timestamp Jan21_23-29-29_2026                                     \
+            --means 0.5410 0.6371 0.4188                                        \
+            --stds 0.1764 0.1650 0.2326                                         \
             --save_misclassified                                                \
             --n_misclassified 10
+            #            --grid_search                                                       \
+
