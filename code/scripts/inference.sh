bfaa18ba4da18b5c8c3c8c4056ff8985b0f799e5
Michele Wiseman <33985124+mswiseman@users.noreply.github.com>
2026-01-25 11:08:46 -0800

    Modify inference script for validation set and parameters
    Updated inference script to use validation set and modified parameters for model loading and thresholds.

--- code/scripts/inference.sh
+++ code/scripts/inference.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+
+
 python ../classification/inference.py                                           \
             --model_path     ../..                                              \
             --dataset_path   ../..                                              \
@@ -7,19 +9,30 @@ python ../classification/inference.py                                           
             --HDF5                                                              \
             --amp                                                               \
             --pretrained                                                        \
-            --set test                                                          \
-            --dual_head                                                         \
-            --inf_gate 0.2                                                      \
-            --up_threshold 0.7                                                  \
-            --down_threshold 0.05                                               \
-            --spor_th 0.85                                                      \
+            --set val                                                           \
             --cuda                                                              \
             --cuda_id 0                                                         \
-            --loading_epoch 43                                                  \
-            --timestamp Jan21_23-29-29_2026                                     \
-            --means 0.5410 0.6371 0.4188                                        \
-            --stds 0.1764 0.1650 0.2326                                         \
+            --loading_epoch 65                                                  \
+            --means 0.5663 0.6596 0.4508                                        \
+            --dual_head                                                         \
+            --stds 0.1811 0.1667 0.2434                                         \
             --save_misclassified                                                \
-            --n_misclassified 10
-            #            --grid_search                                                       \
+            --up_threshold 0.5                                                  \
+            --down_threshold 0.25                                               \
+            --n_misclassified 20                                                \
+            --timestamp Jan22_10-21-00_2026                                     \
+            --grid_search                                                       \
+            --ignore_discard
 
+            #--inf_gate 0.2                                                     \
+            # --spor_th 0.5                                                     \
+
+
+
+# For ResNet Jan22 ep 65 model:
+#  --means 0.5663 0.6596 0.4508 \
+#  --stds 0.1811 0.1667 0.2434
+
+# For ResNet Feb14 ep 59 model:
+#  --means 0.5410 0.6371 0.4188 \
+#  --stds 0.1764 0.1650 0.2326 \
