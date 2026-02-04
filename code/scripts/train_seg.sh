44249647f82f942c0a06a1ac1fd81fa2761a6067
Michele Wiseman <33985124+mswiseman@users.noreply.github.com>
2026-02-04 11:42:32 -0800

    Refactor train_seg.sh for improved execution
    Updated training script to simplify execution and adjust parameters.

--- code/scripts/train_seg.sh
+++ code/scripts/train_seg.sh
@@ -1,25 +1,20 @@
 #!/bin/bash
 
+# note: right now run.py is hardcoded to load patches and masks from a prespecified image_dir and mask_dir
+# need to add arg parse so editing code isn't necessary
 
-for((i=1;i<4;i++))
-do
-    python3 ../segmentation/run.py      \
-                --root_path  /Users/michelewiseman/Desktop/blackbird_ml/ \
-                --model_type DeepLab        \
-                --pretrained                \
-                --save_model                \
-                --weighted_loss             \
-                --loading_epoch 0           \
-                --total_epochs 100          \
-                --cuda                      \
-                --optimType Adam            \
-                --lr 1e-4                   \
-                --weight_decay 2e-4         \
-                --bsize 32                  \
-                --nworker 2                 \
-                --cv                        \
-                --seg_idx $i                \
-                --cuda_device 0
-
-done
-
+python ../segmentation/run.py           \
+        --root_path  /c/Users/Intel\ User/Desktop/blackbird_scripts \
+        --model_type DeepLab            \
+        --pretrained                    \
+        --save_model                    \
+        --weighted_loss                 \
+        --loading_epoch 0               \
+        --total_epochs 95               \
+        --cuda                          \
+        --optimType Adam                \
+        --lr 1e-4                       \
+        --weight_decay 2e-4             \
+        --bsize 32                      \
+        --nworker 1                     \
+        --cuda_device 0
